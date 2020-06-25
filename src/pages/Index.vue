370908998e7d236fa0c11f8b3a176bb4758240d6
Gildas Morel des Vallons <gildas2k@gmail.com>
2020-06-25 18:42:17 +0200

    feat: add q-infinite-scroll and q-pull-to-refresh

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,55 @@
 <template>
     <q-page class="flex flex-center">
-        <img
-            alt="Quasar logo"
-            src="~assets/quasar-logo-full.svg"
-        >
+        <q-infinite-scroll @load="infiniteScroll" :offset="nbItemsPerPage" class="q-mt-xl">
+            <q-pull-to-refresh @refresh="pullToRefresh">
+                <p v-for="block in blocks" :key="block.id">{{ block.message }}</p>
+            </q-pull-to-refresh>
+            <template v-slot:loading>
+                <div class="row justify-center q-my-md">
+                    <q-spinner-dots color="primary" size="40px" />
+                </div>
+            </template>
+        </q-infinite-scroll>
     </q-page>
 </template>
 
 <script>
 export default {
-    name: 'PageIndex'
+    data() {
+        return {
+            nbItemsPerPage: 50,
+            blocks: []
+        }
+    },
+    created() {
+        this.blocks = [
+            ...this.getNewBlocks(0, this.nbItemsPerPage)
+        ];
+    },
+    methods: {
+        getNewBlocks(cursor, nb) {
+            const newBlocks = [];
+            for (let i = cursor + 1; i <= cursor + nb; i++) {
+                newBlocks.push({ id: i, message: `Line ${i}` });
+            }
+
+            return newBlocks;
+        },
+        pullToRefresh(done) {
+            setTimeout(() => {
+                this.blocks = this.blocks.splice(0, this.nbItemsPerPage);
+                done();
+            }, 1500);
+        },
+        infiniteScroll(index, done) {
+            setTimeout(() => {
+                this.blocks = [
+                    ...this.blocks,
+                    ...this.getNewBlocks(this.blocks.length, this.nbItemsPerPage)
+                ];
+                done();
+            }, 1500);
+        }
+    }
 }
 </script>
