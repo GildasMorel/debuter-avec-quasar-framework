5390da132a0be0dcdf6c2a0ffd14b5164bf0dda2
Gildas Morel des Vallons <gildas2k@gmail.com>
2020-06-25 09:22:52 +0200

    feat: add q-table

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,48 @@
 <template>
     <q-page class="flex flex-center">
-        <img
-            alt="Quasar logo"
-            src="~assets/quasar-logo-full.svg"
-        >
+        <q-table
+            title="Fichiers"
+            :data="data"
+            :columns="columns"
+            row-key="filename"
+        />
     </q-page>
 </template>
 
 <script>
+import { format } from 'quasar';
+const { humanStorageSize } = format;
+
 export default {
-    name: 'PageIndex'
+    data () {
+        return {
+            columns: [
+                {
+                    name: 'filename',
+                    label: 'Nom du fichier',
+                    align: 'left', // 'right' par défaut
+                    field: row => row.filename,
+                    sortable: true
+                },
+                {
+                    name: 'storageSize',
+                    label: 'Poids du fichier',
+                    field: row => row.storageSize,
+                    format: val => val ? humanStorageSize(val) : val,
+                    sortable: true
+                }
+            ],
+            data: [
+                {
+                    filename: 'readme.md',
+                    storageSize: 15000
+                },
+                {
+                    filename: 'demo.avi',
+                    storageSize: 237000000
+                }
+            ]
+        };
+    }
 }
 </script>
